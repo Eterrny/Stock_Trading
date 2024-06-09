831865e01dd55eded826c29f0c8bf63436b4cbbe
Eterrny <g.d.2001@mail.ru>
2024-06-09 09:12:27 +0400

    Initial Commit

---
+++ src/main/java/ru/ssu/springproject/stocks_trading/StocksTradingApplication.java
@@ -0,0 +1,13 @@
+package ru.ssu.springproject.stocks_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksTradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ssu/springproject/stocks_trading/StocksTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssu.springproject.stocks_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
